d1ccd0f923a6de6d6c53bade7f9728e77563af5c
skarbus67 <wroclawska148@gmail.com>
2026-05-05 11:45:11 +0200

    feat: get endpoint that usues created clients

--- backend/main.go
+++ backend/main.go
@@ -1,10 +1,39 @@
 package main
 
 import (
+	"log"
+	"net/http"
 	"github.com/gin-gonic/gin"
 )
 
 func main(){
 	router := gin.Default()
 
+	router.GET("/dog", func(c *gin.Context){
+
+		pictureUrl, err := GetPicture()
+		if(err != nil){
+			log.Printf("critical error : %v", err)
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": "internal server error",
+			})
+			return
+		}
+
+		fact, err := GetFact()
+		if(err != nil){
+			log.Printf("critical error : %v", err)
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": "internal server error",
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"url": pictureUrl,
+			"fact": fact,
+		})
+	})
+
+router.Run(":8080")
 }
