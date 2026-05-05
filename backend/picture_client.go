a1293eef499da9ba902c4303ae4e4aa6cdb78986
skarbus67 <wroclawska148@gmail.com>
2026-05-05 11:44:04 +0200

    feat: dog picture client

---
+++ backend/picture_client.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"fmt"
+)
+
+type pictureApiResponse struct{
+	Message string `json:"message"`
+	Status string `json:"status"`
+}
+
+func GetPicture() (string, error) {
+	url := "https://dog.ceo/api/breeds/image/random"
+
+	response, err := http.Get(url)
+	
+	if err != nil {
+		return "", fmt.Errorf("failed fetching dog picture : %w", err)
+	}
+
+	defer response.Body.Close()
+
+	if(response.StatusCode != 200){
+		return "", fmt.Errorf("wrong status code : %v", response.StatusCode)
+	}
+
+	var result pictureApiResponse
+
+	err = json.NewDecoder(response.Body).Decode(&result)
+
+	if err != nil {
+		return "", fmt.Errorf("failed decoding json : %w", err)
+	}
+
+	return result.Message, nil
+
+}
