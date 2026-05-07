6ba58451549c4823c1c5eca53bc83bc8fbd68637
skarbus67 <wroclawska148@gmail.com>
2026-05-07 21:11:31 +0200

    refactor: use goroutines and channels for parallel data fetching

--- backend/main.go
+++ backend/main.go
@@ -8,6 +8,11 @@ import (
 	"dog-app/clients/dogpic"
 )
 
+type response struct{
+		res string
+		err error
+}
+
 func main(){
 	router := gin.Default()
 
@@ -15,26 +20,42 @@ func main(){
 
 	router.GET("/api/dog", func(c *gin.Context){
 
-		pictureUrl, err := dogpic.GetPicture()
-		if(err != nil){
-			log.Printf("critical error : %v", err)
-			c.JSON(http.StatusInternalServerError, gin.H{
-				"error": "internal server error",
-			})
-			return
-		}
+		chPic := make (chan response, 1)
+		chFact := make (chan response, 1)
 
-		fact, err := dogfact.GetFact()
-		if(err != nil){
-			log.Printf("critical error : %v", err)
-			c.JSON(http.StatusInternalServerError, gin.H{
-				"error": "internal server error",
-			})
-			return
+		go func(){
+			pictureUrl, err := dogpic.GetPicture()
+			chPic <- response{res: pictureUrl, err: err}
+		}()
+
+		go func(){
+			fact, err := dogfact.GetFact()
+			chFact <- response{res: fact, err: err}
+		}()
+
+		var fact string
+        var picURL string
+		for i := 0; i < 2; i++ {
+    		select {
+				case f := <-chFact:
+        			if f.err != nil {
+            			log.Printf("Fact error: %v", f.err)
+            			c.JSON(http.StatusInternalServerError, gin.H{"error": "fact failed"})
+            			return
+        			}
+					fact = f.res
+    			case p := <-chPic:
+        			if p.err != nil {
+            			log.Printf("Pic error: %v", p.err)
+            			c.JSON(http.StatusInternalServerError, gin.H{"error": "pic failed"})
+            			return
+        			}
+					picURL = p.res
+			}
 		}
 
 		c.JSON(http.StatusOK, gin.H{
-			"url": pictureUrl,
+			"url": picURL,
 			"fact": fact,
 		})
 	})
